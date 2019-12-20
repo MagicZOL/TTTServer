44e01b5769e476721994fe85260dd853e871d606
MagicZL0 <tnwls9721@naver.com>
2019-12-20 14:12:36 +0900

    userid err modifity

--- routes/chat.js
+++ routes/chat.js
@@ -15,32 +15,42 @@ router.post('/add', function(req, res, next)
     return;
   }
 
-  //1.유저 이름 찾기
-  var usersCollection = db.collection('users');
+  var userId = req.session.user_id;
 
-  usersCollection.findOne({_id : mongodb.ObjectID(req.session.user_id)}, {projection : {_id : false, name : true}}, function(err, usersResult)
+  if(userId)
   {
-    if(err) throw(err);
+      //1.유저 이름 찾기
+    var usersCollection = db.collection('users');
 
-    //2.메시지의 Seq 찾기
-    var counterCollection = db.collection("counter");
-    counterCollection.findOneAndUpdate({_id: "chatcounter"}, {$inc : {seq: 1}},{returnOriginal:false}, function(err, result)
+    usersCollection.findOne({_id : mongodb.ObjectID(req.session.user_id)}, {projection : {_id : false, name : true}}, function(err, usersResult)
     {
-      if(err) throw (err);
+      if(err) throw(err);
 
-      console.log(result);
-
-      var chatCollection = db.collection("chat");
-
-      //3. chat collection에 메시지 추가하기
-      //날짜 불러오기 date : Date()
-      chatCollection.insertOne({_id : result.value.seq, message : message, name : usersResult.name, date : Date() }, function(err, result) 
+      //2.메시지의 Seq 찾기
+      var counterCollection = db.collection("counter");
+      counterCollection.findOneAndUpdate({_id: "chatcounter"}, {$inc : {seq: 1}},{returnOriginal:false}, function(err, result)
       {
-        if(err) throw err;
-        res.status(200).json({message : "OK"});
+        if(err) throw (err);
+
+        console.log(result);
+
+        var chatCollection = db.collection("chat");
+
+        //3. chat collection에 메시지 추가하기
+        //날짜 불러오기 date : Date()
+        chatCollection.insertOne({_id : result.value.seq, message : message, name : usersResult.name, date : Date() }, function(err, result) 
+        {
+          if(err) throw err;
+          res.status(200).json({message : "OK"});
+        });
       });
     });
-  });
+  }
+
+  else
+  {
+    res.status(401).send('Undefine')
+  }
 });
 
 /*서버에서 클라이언트로 메시지 전송*/
