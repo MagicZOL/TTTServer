ed8b93dff69ba7e02ec66fc405c85cdf75e75f6d
MagicZL0 <tnwls9721@naver.com>
2019-12-27 17:57:02 +0900

    Update game.js

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ module.exports = function(server)
             transports : ['websocket'],
         });
 
+        //socket에는 연결한 클라이언트의 정보를 가지고 있다.
         io.on('connection', function(socket)
         {
             console.log('Connect');
@@ -31,6 +32,8 @@ module.exports = function(server)
                 {
                     var room = { roomId: roomId, clients: [{ clientId : socket.id, ready : false }]};
                     rooms.push(room);
+
+                    socket.emit("join", {roomId : roomId, clientId : socket.id});
                 });
             }
 
@@ -61,6 +64,8 @@ module.exports = function(server)
                 {
                     var client = { clientId : socket.id, ready : false }
                     rooms[roomIndex].clients.push(client);
+
+                    socket.emit("join", {roomId : rooms[roomIndex].roomId, clientId : socket.id});
                 });
             }
             else
@@ -69,6 +74,90 @@ module.exports = function(server)
                 createRoom();
 
             }
+
+
+            //클라이언트가 Ready되면 호출되는 이벤트
+            socket.on('ready', function(data) 
+            {
+                if(!data) return;
+                
+                //find함수 안에는 함수를 넣을 수 있는데 rooms가 가지고 있는 요소중 하나를 찾기 위한 함수
+                var room = rooms.find(room => room.roomId === data.roomId);
+
+                if(room)
+                {
+                    var clients = room.clients;
+                    var client = clients.find(client =>client.clientId === data.clientId)
+                    if(client) client.ready=true;
+
+                    //방안에 모두가 true이면 게임시작
+                    if(clients.length == 2)
+                    {
+                        if(clients[0].ready == true && clients[1].ready == true)
+                        {
+                            //io.in(room.roomId).emit('play', {first : clients[0].clientId});
+                            io.to(clients[0].clientId).emit('play', {first : true});
+                            io.to(clients[1].clientId).emit('play', {first : false});
+                        }
+                    }
+                }
+                
+                // var room = rooms.find((room) => 
+                // {
+                //     if(room.roomId === data.roomId)
+                //     {
+                //         return true;
+                //     }
+                //     return false;
+                // });
+
+                // for(var i =0; i<rooms.length; i++)
+                // {
+                //     if(rooms[i].roomId == data.roomId)
+                //     {
+                //         var clients = rooms[i].clients;
+                //         for(var j=0; j<clients.length; j++)
+                //         {
+                //             var client = clients[j];
+                //             if(client.clientId == data.clientId)
+                //             {
+                //                 client.ready = true;
+                //             }
+                //         }
+                //     }
+                // }
+            });
+
+            socket.on('select', function(data)
+            {
+                if(!data) return;
+                var index = data.index;
+                var roomId = data.roomId;
+
+                if(index > -1 && roomId)
+                {
+                    socket.to(roomId).emit('selected', {index : index});
+                }
+            });
+
+            socket.on('win', function(data)
+            {
+
+            });
+            
+            socket.on('tie', function(data)
+            {
+                
+            });
+            //테스트
+            //메시지를 보낼때 emit라는 함수를 사용한다 socket.emit("이벤트이름", "객체(키, 값")
+            //socket.emit('test', {message : '안녕하세여'});
+
+            //이벤트를 정의하는 함수
+            //socket.on("hello", function(data)
+            // {
+            //     console.log(data.msg);
+            // });
 
             socket.on('disconnect', function(reason)
             {
